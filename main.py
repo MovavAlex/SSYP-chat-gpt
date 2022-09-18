5f1b5c28a34d83f183986552bbf6b9422a7887a2
MovavAlex <93071401+MovavAlex@users.noreply.github.com>
2022-09-18 15:44:07 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+
+#------------------------------------#
+#x = input("Имя: ")#
+#z = input('Возраст:')#
+#v = input('Время:')
+#
+#print('Меня зовут:', x,"Мне:", z, "лет,сейчас уже:",v)#
+
+
+
+#a = int(input("a:"))
+
+#b = int(input("b:"))
+
+#s = a * b
+#p = (a + b) * 2
+#print("s=",s, "p=", p)
+
+
+#a = input('a')
+#b = input('b')
+
+#a,b = b,a
+
+#print(a)
+#print(b)
+
+
+#a = int(input("a"))
+#b = int(input("b"))
+#c = (a+b)/2
+
+#print(c)
+
+
+
+#h = int(input("часы"))
+#m = int(input("мин"))
+#s = int(input("сек"))
+
+#c=(h * 60 * 60) + (m*60)+ s
+
+
+#print('С начала дня прошло: ', c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
